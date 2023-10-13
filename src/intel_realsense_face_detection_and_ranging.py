3a755247cf8679e421ca099da03c5260082ab11c
Gill-Singh-A <amansg22@iitk.ac.in>
2023-10-13 15:39:16 +0530

    Implemented Calculation and Displaying of Coordinates of Detected Faces

--- src/intel_realsense_face_detection_and_ranging.py
+++ src/intel_realsense_face_detection_and_ranging.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 
-import rospy, cv2, numpy, math, image_geometry
+import rospy, cv2, numpy, image_geometry
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
 from sensor_msgs.msg import CameraInfo
-from geometry_msgs.msg import Point
 
 bridge = CvBridge()
 camera_model = image_geometry.PinholeCameraModel()
@@ -12,14 +11,18 @@ camera_model = image_geometry.PinholeCameraModel()
 cameraInfo = None
 image = None
 depth_image = None
-ray = None
+distance_range = 0.35
 
 GREEN = (0, 255, 0)
+WHITE = (255, 255, 255)
 
 scale_factor = 1.3
 min_neighbors = 5
 cascade_file = "xml/haarcascade_frontalface_default.xml"
 
+def getCameraInfo(camera_info):
+    global cameraInfo
+    cameraInfo = camera_info
 def getImage(ros_image):
     global image
     image = cv2.cvtColor(bridge.imgmsg_to_cv2(ros_image), cv2.COLOR_RGB2BGR)
@@ -27,20 +30,28 @@ def getDepthImage(ros_depth_image):
     global depth_image
     depth_image = bridge.imgmsg_to_cv2(ros_depth_image, ros_depth_image.encoding)
 def getRay(point):
-    global ray
-    ray = numpy.array(camera_model.projectPixelTo3dRay(point))
-def getCameraInfo(camera_info):
-    global cameraInfo
-    cameraInfo = camera_info
+    return numpy.array(camera_model.projectPixelTo3dRay(point))
 
-def draw_faces(image, faces):
-    for x, y, w, h in faces:
+def measureFaceDepth(face):
+    x, y, w, h = face
+    length = w * h
+    depth_slice = int((1-distance_range)*length//2)
+    face_depth = numpy.sort(depth_image[x:x+w, y:y+h].flatten())[depth_slice: -depth_slice]
+    return numpy.sun(face_depth)/len(face_depth)
+def draw_faces(image, faces, ranging=True):
+    for face in faces:
+        x, y, w, h = face
         cv2.rectangle(image, (x, y), (x+w, y+h), GREEN, 2)
-def detect_faces(image, face_classifier, scale_factor, min_neighbors, localize=True):
+        if ranging == True:
+            ray = getRay([x+w//2, y+h//2])
+            depth = measureFaceDepth(face)
+            coordinates = ray * depth
+            cv2.putText(image, f"({coordinates[0]},{coordinates[1]},{coordinates[2]})mm", (x, y+h), cv2.FONT_HERSHEY_COMPLEX, 1, WHITE, 2)
+def detect_faces(image, face_classifier, scale_factor, min_neighbors, localize=True, ranging=True):
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     faces = face_classifier.detectMultiScale(gray_image, scaleFactor=scale_factor, minNeighbors=min_neighbors)
     if localize:
-        draw_faces(image, faces)
+        draw_faces(image, faces, ranging=ranging)
     return faces
 
 if __name__ == "__main__":
@@ -56,6 +67,6 @@ if __name__ == "__main__":
         rate.sleep()
     face_classifier = cv2.CascadeClassifier(cascade_file)
     while not rospy.is_shutdown() and cv2.waitKey(1) != ord('q'):
-        faces = detect_faces(image, face_classifier, scale_factor, min_neighbors, localize=True)
-        cv2.imshow("Image", image)
+        faces = detect_faces(image, face_classifier, scale_factor, min_neighbors, localize=True, ranging=True)
+        cv2.imshow("Face Detection and Ranging", image)
     cv2.destroyAllWindows()
