9bca284ba8222d09dbb4325daf2ea00a3325e56c
Gill-Singh-A <amansg22@iitk.ac.in>
2023-10-13 15:15:07 +0530

    Added Required Libraries

--- src/intel_realsense_face_detection_and_ranging.py
+++ src/intel_realsense_face_detection_and_ranging.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
+import rospy, cv2, numpy, math, image_geometry
+from cv_bridge import CvBridge
+from sensor_msgs.msg import Image
+from sensor_msgs.msg import CameraInfo
+from geometry_msgs.msg import Point
+
 if __name__ == "__main__":
     pass
